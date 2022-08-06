342523dc3d1a0f6f608ed882a4356d6c344c0ab0
Stephen Wilkens <stephenwilkens@gmail.com>
2022-08-06 13:46:25 -0600

    add geocoder service and weather service with passing tests

--- app/services/geocoder_service.rb
+++ app/services/geocoder_service.rb
@@ -1,6 +1,6 @@
 class GeocoderService
   def self.get_coordinates(location)
-    response = BaseService.geocoder_connection
+    response = BaseService.mapquest_connection
     .get("/geocoding/v1/address/?key=#{ENV['MAPQUEST_KEY']}&location=#{location}", content_type: 'application/json')
     BaseService.get_json(response)
   end
