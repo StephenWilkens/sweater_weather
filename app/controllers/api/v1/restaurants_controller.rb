7332e9b166406905958db9e1919d9e88af260f84
Stephen Wilkens <stephenwilkens@gmail.com>
2022-08-08 11:00:25 -0600

    add restaurants controllers show action request spec, all tests passing cov at 100

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -3,7 +3,6 @@ class Api::V1::RestaurantsController < ApplicationController
     ll = GeocoderFacade.get_coordinates(params[:location])
     weather = WeatherFacade.get_weather(ll.lat, ll.lng)
     restaurant = YelpFacade.get_nearby_restaurant(params[:location], params[:food])
-    x = render json: RestaurantSerializer.new(params[:location], weather, restaurant)
-    require "pry"; binding.pry
+    render json: RestaurantSerializer.new(params[:location], weather, restaurant)
   end
 end
---
+++ spec/requests/api/v1/restaurants/show_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe 'Restaurants Show Action' do
+  describe 'happy path' do
+    it 'returns the city, weather summary, and restaurants name and address', :vcr do
+      get '/api/v1/munchies?location=denver,co&food=chinese'
+
+      expect(response).to be_successful
+      rb = JSON.parse(response.body, symbolize_names: true)
+      munchies = rb[:data]
+
+      expect(munchies).to be_a Hash
+      expect(munchies).to have_key(:id)
+      expect(munchies[:id]).to eq(nil)
+      expect(munchies).to have_key(:type)
+      expect(munchies[:type]).to eq("munchie")
+      expect(munchies).to have_key(:attributes)
+      expect(munchies[:attributes]).to have_key(:destination_city)
+      expect(munchies[:attributes]).to have_key(:forecast)
+      expect(munchies[:attributes][:forecast]).to have_key(:summary)
+      expect(munchies[:attributes][:forecast]).to have_key(:temperature)
+      expect(munchies[:attributes]).to have_key(:restaurant)
+      expect(munchies[:attributes][:restaurant]).to have_key(:name)
+      expect(munchies[:attributes][:restaurant]).to have_key(:address)
+    end
+  end
+end
