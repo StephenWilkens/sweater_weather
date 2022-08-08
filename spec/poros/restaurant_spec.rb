55ffca00f83937da4b21523fc341aec0105d6bb0
Stephen Wilkens <stephenwilkens@gmail.com>
2022-08-08 10:15:35 -0600

    working on facade test, poro test passing

---
+++ spec/poros/restaurant_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Restaurant do
+  it 'exists and has attributes', :vcr do
+    location = 'Denver, CO'
+    category = 'Chinese'
+    json = YelpService.get_restaurant(location, category)
+    data = json[:businesses][0]
+    restaurant = Restaurant.new(data)
+
+    expect(restaurant).to be_an_instance_of(Restaurant)
+    expect(restaurant.name).to eq(json[:businesses][0][:name])
+    expect(restaurant.address).to eq(json[:businesses][0][:location][:display_address])
+  end
+end
