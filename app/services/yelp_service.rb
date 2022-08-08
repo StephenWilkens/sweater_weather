462957ae0eb1b7559882031c0c7c1f3111b1e61a
Stephen Wilkens <stephenwilkens@gmail.com>
2022-08-08 09:45:50 -0600

    writing service spec

--- app/services/yelp_service.rb
+++ app/services/yelp_service.rb
@@ -1,9 +1,9 @@
 class YelpService
   def self.get_restaurant(location, category)
-    response = BaseService.yelp_connection.get('v3/businesses/search') do |f|
-      f.headers['authorization'] = ENV['YELP_KEY']
+    response = BaseService.yelp_connection.get('v3/businesses/search?') do |f|
+      f.headers['Authorization'] = ENV['YELP_KEY']
       f.params['location'] = location
-      f.params['category'] = category
+      f.params['categories'] = category
     end
   end
 end
--- spec/services/yelp_service_spec.rb
+++ spec/services/yelp_service_spec.rb
@@ -1,6 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe YelpService do
+  context 'get restaurants' do
+    it 'returns a restaurant based off location and category', :vcr do
+      location = 'Denver, CO'
+      category = 'Chinese'
+      response = YelpService.get_restaurant(location, category)
+      require "pry"; binding.pry
+      restaurant = response[:businesses][0]
 
-
+      expect(response).to be_a(Hash)
+      expect(response[:businesses]).to be_a(Array)
+      expect(response[:businesses][0]).to be_a(Hash)
+      expect(response[:businesses][0]).to have_key(:id)
+      expect(response[:businesses][0]).to have_key(:alias)
+      expect(response[:businesses][0]).to have_key(:name)
+      expect(response[:businesses][0]).to have_key(:image_url)
+      expect(response[:businesses][0]).to have_key(:is_closed)
+      expect(response[:businesses][0]).to have_key(:url)
+      expect(response[:businesses][0]).to have_key(:review_count)
+      expect(response[:businesses][0]).to have_key(:categories)
+      expect(response[:businesses][0][:categories][0]).to have_key(:alias)
+      expect(response[:businesses][0][:categories][0]).to have_key(:title)
+      expect(response[:businesses][0]).to have_key(:rating)
+      expect(response[:businesses][0]).to have_key(:coordinates)
+      expect(response[:businesses][0][:coordinates]).to have_key(:latitude)
+      expect(response[:businesses][0][:coordinates]).to have_key(:longitude)
+      expect(response[:businesses][0]).to have_key(:transactions)
+      expect(response[:businesses][0]).to have_key(:price)
+      expect(response[:businesses][0]).to have_key(:location)
+      expect(response[:businesses][0][:location]).to have_key(:address1)
+      expect(response[:businesses][0][:location]).to have_key(:address2)
+      expect(response[:businesses][0][:location]).to have_key(:address3)
+      expect(response[:businesses][0][:location]).to have_key(:city)
+      expect(response[:businesses][0][:location]).to have_key(:zip_code)
+      expect(response[:businesses][0][:location]).to have_key(:country)
+      expect(response[:businesses][0][:location]).to have_key(:state)
+      expect(response[:businesses][0][:location]).to have_key(:display_address)
+      expect(response[:businesses][0]).to have_key(:phone)
+      expect(response[:businesses][0]).to have_key(:display_phone)
+      expect(response[:businesses][0]).to have_key(:distance)
+    end
+  end
 end
