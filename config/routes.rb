cd89efcb6abef9c8adf9132fb4d483e359331a32
Stephen Wilkens <stephenwilkens@gmail.com>
2022-08-09 17:54:55 -0600

    changed users show to sessions create

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
       get '/forecast', to: 'forecasts#index'
       get '/munchies', to: 'restaurants#show'
       post '/users', to: 'users#create'
-      post '/sessions', to: 'users#show'
+      post '/sessions', to: 'sessions#create'
       post '/road_trip', to: 'road_trips#show'
     end
   end
