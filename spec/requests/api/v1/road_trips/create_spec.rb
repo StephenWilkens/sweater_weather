f422e1724ee8d18301e5c8706f60b2606a378a14
Stephen Wilkens <stephenwilkens@gmail.com>
2022-08-10 12:14:04 -0600

    add impossible route serializer, logic in facade, and logic in controller. Tested in road trip create req spec

--- spec/requests/api/v1/road_trips/create_spec.rb
+++ spec/requests/api/v1/road_trips/create_spec.rb
@@ -44,5 +44,32 @@ RSpec.describe 'Road Trips create action' do
       expect(response).to_not be_successful
       expect(response.status).to eq(401)
     end
+
+    it 'returns an impossible route json response if the route is impossible', :vcr do
+      bob = User.create!(email: "bob@gmail.com", password: "abc123", password_confirmation: "abc123")
+      bob.gen_api_key
+      body = {
+               "origin": "Denver,CO",
+               "destination": "London, UK",
+               "api_key": "#{bob.api_key}"
+             }
+      headers = { 'CONTENT_TYPE' => 'application/json', 'Accept' => 'application/json' }
+
+      post '/api/v1/road_trip', headers: headers, params: JSON.generate(body)
+
+      expect(response).to be_successful
+      rb = JSON.parse(response.body, symbolize_names: true)
+
+      expect(rb).to have_key(:data)
+      expect(rb[:data]).to have_key(:type)
+      expect(rb[:data]).to have_key(:id)
+      expect(rb[:data]).to have_key(:attributes)
+      expect(rb[:data][:attributes]).to have_key(:start_city)
+      expect(rb[:data][:attributes]).to have_key(:end_city)
+      expect(rb[:data][:attributes]).to have_key(:travel_time)
+      expect(rb[:data][:attributes][:travel_time]).to eq('Impossible')
+      expect(rb[:data][:attributes]).to have_key(:weather_at_eta)
+      expect(rb[:data][:attributes][:weather_at_eta]).to eq({})
+    end
   end
 end
